fa0348ba6a1c00235314f0f63ec5aaf399ab6c7a
erias04 <lichtbargeld@gmail.com>
2021-09-08 21:58:48 +0200

    feat: views.py: added dummy posts to test it Added two posts (a dictionary) to test the posts and the appearance.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'erias',
+        'title': 'Blog Post 1',
+        'content': 'First post content just a dummy post to test it',
+        'date_posted': 'September 8, 2021 22:00'
+    },
+    {
+        'author': 'erias',
+        'title': 'Blog Post 2',
+        'content': 'Second post content just a dummy',
+        'date_posted': 'September 8, 2021 22:00'
+    }
+]
+
+
 def home(request):
     return render(request, 'blog/home.html')
 
