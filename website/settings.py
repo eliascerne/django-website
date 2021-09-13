2e1b21eb47fd872b9aeb437ad435ccd0228363fd
erias04 <lichtbargeld@gmail.com>
2021-09-13 16:51:57 +0200

    changed django-secret-key to secure the secret key Added import os and dotenv (installed with pip3 install python-dotenv) and set the path to .env and find there the SECRET_KEY.
    From: https://dev.to/vladyslavnua/how-to-protect-your-django-secret-and-oauth-keys-53fl
    
    The .env is in the .gitignore so nobody will ever see my secret key over github.

--- website/settings.py
+++ website/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+#dotenv is used to hide the secret key
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tvel06wi91&y2hk%k5+46(x-3vr9)jmj#2*o2pkwy8rt%t7e_w'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
