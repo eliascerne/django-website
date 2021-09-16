f5c26a749ec1221deeec328447b7aaf3321a7989
erias04 <lichtbargeld@gmail.com>
2021-09-16 22:21:43 +0200

    program the navToggle (shows nav_menu) In javascript we on the top got our constants from html id and then uses them with an if statment. I added an eventlistener which listens for a event (in our case) click and then add a new class to html called 'show-menu'. We nedd to write show-menu in css.

--- eriasoverflow/static/eriasoverflow/js/main.js
+++ eriasoverflow/static/eriasoverflow/js/main.js
@@ -2,3 +2,10 @@
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
+
+/* SHOW MENU */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    })
+}
